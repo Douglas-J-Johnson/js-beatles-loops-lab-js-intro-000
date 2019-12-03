94a2c6ef0852d96e5c6cf90fc0d7a0ba535a8c25
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-03 17:54:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var phrases = []
+  
+  for (i=0; i<musicians.length; i++) {
+    phrases[i] = `${musicians[i]} plays ${instruments[i]}`
+  }
+
+  return phrases
+}
