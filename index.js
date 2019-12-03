ecf1ed7409ea296407af4bd2d1715aa0e9cb8f77
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-03 18:03:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts (facts) {
   var i = 0
   var exclaimedFacts = []
-  
+
   while (i<facts.length) {
     exclaimedFacts[i] = `${facts[i]}!!!`
   }
